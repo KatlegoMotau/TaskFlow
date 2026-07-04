d70931356af9068c91d4f8f26e5310506d5380a8
Katlego Motau <katlegomotau634@gmail.com>
2026-07-04 16:41:25 +0200

    Delete taskflow directory

--- taskflow/src/main/java/com/katlego/taskflow/entity/Role.java
+++
@@ -1,6 +0,0 @@
-package com.katlego.taskflow.entity;
-
-public enum Role {
-    USER,
-    ADMIN
-}
--- taskflow/src/main/java/com/katlego/taskflow/entity/Status.java
+++
@@ -1,7 +0,0 @@
-package com.katlego.taskflow.entity;
-
-public enum Status {
-    TODO,
-    IN_PROGRESS,
-    COMPLETED
-}
